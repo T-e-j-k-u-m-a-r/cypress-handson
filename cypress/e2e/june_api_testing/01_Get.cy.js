0d9fc13bff571e9effbb267a7f0334735c2bdf6f
Tejkumar Kempaiah <connect.tejkumar@gmail.com>
2025-06-27 06:40:15 +0530

    Added get call file

---
+++ cypress/e2e/june_api_testing/01_Get.cy.js
@@ -0,0 +1,57 @@
+// Endpoint: https://faux-api.com/serve/courses_6076335750361604
+
+describe(`API Testing`,function(){
+
+    it(`Get Call - Method 1`,function(){
+
+      cy.request(`https://faux-api.com/serve/courses_6076335750361604`).then(function(interception){
+
+        expect(interception.status).to.eq(200)
+        expect(interception.body.status).to.eq(`success`)
+        expect(interception.body.result).have.lengthOf(5)
+        
+        //String representation of the JSON Response Object
+        cy.log(JSON.stringify(interception.body));
+        
+
+      })
+
+    })
+
+    it(`Get Call - Method 2`,function(){
+
+        cy.request({
+            method:`GET`,
+            url:`https://faux-api.com/serve/courses_6076335750361604`
+        }).then(function(interception){
+                       
+        expect(interception.status).to.eq(200)
+        expect(interception.body.status).to.eq(`success`)
+        expect(interception.body.result).have.lengthOf(5)
+        
+        //String representation of the JSON Response Object
+        cy.log(JSON.stringify(interception.body));
+        
+
+        })
+
+    })
+
+    it(`Get Call - With Query Params`,function(){
+      cy.request({
+        method:`GET`,
+        url: `https://faux-api.com/serve/courses_6076335750361604`,
+        qs:{
+          courseName:"javascript"
+        }
+      }).then(function(interception){
+
+        expect(interception.status).eq(200)
+        cy.log(JSON.stringify(interception.body))
+        
+      })
+
+    })
+
+
+})
